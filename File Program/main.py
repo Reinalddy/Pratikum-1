bd3925199d15efd2e89aadc047cb6759188d83cb
Reinalddy <Reinalddyaldy183@gmail.com>
2021-10-23 19:58:28 +0700

    menambahkan keterangan atau commet

--- File Program/main.py
+++ File Program/main.py
@@ -1,23 +1,30 @@
 print ("Assalamualaikum")
 print ("Let me introduce my self")
 
+#nama
 data = input("My Name Is:")
 print("Nama Lengkap = ",data,)
 
+#nama panggilan
 data = input("But you can call me:")
 print("Nama Panggilan = ",data,)
 
+#NPM
 data = input("My NPM Is:")
 print("npm=",data)
 
+#Tempat Tanggal Lahir
 data = input("I Was Born in:")
 print("Tempat lahir = ",data,)
 
+#Umur
 data_int = int(input("umur: "))
 print("umur = ",data_int,"years old",)
 
+#Alamat
 data = input("iam very glad if you want to invite my house in:")
 print("Alamat =",data)
 
+#No Telepon
 data_int = int(input("so, dont forget to call me before with the number:"))
 print("no telepon:" ,data_int)
